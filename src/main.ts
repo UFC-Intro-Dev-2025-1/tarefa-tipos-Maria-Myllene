9ad50034b289d8d52ba95391f2303722a8617ec2
Myllene Barbosa <mariamyllene130@gmail.com>
2025-05-23 22:31:12 +0000

    feat: tarefa concluída

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,30 @@
 // Escreva seu código aqui!
+import readlineSync from 'readline-sync';
+
+
+let janelas: number = 4;
+console.log('Há ' + janelas + ' janelas no laboratório.');
+
+let arCondicionado: string = "frio";
+console.log(`O ar-condicionado do laboratório é ${arCondicionado}.`);
+
+let entradasLab;
+console.log(entradasLab); // Isso se refere as entradas laterais do laboratório, entre as portas, do lado contrário as janelas que, para mim, não fazem sentido algum
+
+let tickets: boolean = false;
+console.log(`Os tickets de acompanhamento da Lana são em vermelho, amarelo e azul.\n ${tickets}`);
+
+type poteDeMarmita = {
+    cor: string;
+    formato: string;
+    reparticoesInternas: number;
+    talheres: boolean;
+}
+let poteDeMarmita = {
+    cor: "laranja",
+    formato: "retangular",
+    reparticoesInternas: 2,
+    talheres: true,
+};
+console.log(`O pote de marmita da Myllene é ${poteDeMarmita.cor}, possui um formato ${poteDeMarmita.formato}, tem ${poteDeMarmita.reparticoesInternas} repartições internas e possui talheres (${poteDeMarmita.talheres}).`);
+
